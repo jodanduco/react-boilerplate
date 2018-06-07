07757513f33c48e9cf493887c6142b6404f09aee
Josue Duran <josue.duran@globant.com>
2018-06-07 14:05:39 -0500

    add heltmet test

--- app/containers/FeaturePage/tests/index.test.js
+++ app/containers/FeaturePage/tests/index.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { shallow } from 'enzyme';
 import { FormattedMessage } from 'react-intl';
 
 import H1 from 'components/H1';
+import List from 'components/List';
 import messages from '../messages';
 import FeaturePage from '../index';
 
@@ -15,6 +17,33 @@ describe('<FeaturePage />', () => {
       <H1>
         <FormattedMessage {...messages.header} />
       </H1>
+    )).toBe(true);
+  });
+
+  it('should render <Helmet> with correct title and meta', () => {
+    const helmetTitle = 'Feature Page';
+    const metaHelmet = {
+      name: 'description',
+      content: 'Feature page of React.js Boilerplate application',
+    };
+    const renderedComponent = shallow(
+      <FeaturePage />
+    );
+    expect(renderedComponent.contains(
+      <Helmet>
+        <title>{helmetTitle}</title>
+        <meta name={metaHelmet.name} content={metaHelmet.content} />
+      </Helmet>
+    )).toBe(true);
+  });
+
+  it('should cointains a <List> component', () => {
+    const renderedComponent = shallow(
+      <FeaturePage />
+    );
+    expect(renderedComponent.contains(
+      <List>
+      </List>
     )).toBe(true);
   });
 
